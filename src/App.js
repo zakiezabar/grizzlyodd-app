78553a33a96e9cb15c5eed5938a9f1400a4b50fc
zakiezabar <zakiezabar@gmail.com>
2021-08-31 23:34:59 +0800

    updated menu-items to navigate withRouter HOC, higher order component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 
+const DesignKit = () => (
+  <div>
+    <h1>Design Kit</h1>
+    <span>Free for all</span>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/' component={DesignKit} />
+      </Switch>
     </div>
   );
 }
